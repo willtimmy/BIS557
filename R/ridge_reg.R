75dfb51b2d4e3f13394501deb8ad4688ed14cb5d
Qi Tan <qi.tan@yale.edu>
2018-10-15 12:17:18 -0400

    20181015

--- R/ridge_reg.R
+++ R/ridge_reg.R
@@ -13,8 +13,8 @@ ridge_reg <- function(formula, lambda, data){
   rownames(data) <- NULL 
   #B/c pima already has rownames, since training set and test set does not match the rownames and returns error
   m <- model.matrix(formula, data)
-  y <- matrix(data[, as.character(formula)[2]], ncol = 1)
-  y <- y[as.numeric(rownames(m)), , drop = FALSE]
+  y <- matrix(data[, as.character(formula)[2]], ncol=1)
+  y <- y[as.numeric(rownames(m)), , drop=FALSE]
  
   #Via SVD:
   svd_obj <- svd(m)
@@ -25,7 +25,7 @@ ridge_reg <- function(formula, lambda, data){
   D <- diag(svals / (svals^2 + lambda))
   beta <- V %*% D %*% t(U) %*% y
   rownames(beta) <- colnames(m)
-  ret <- list(coefficients = beta, lambda = lambda, formula = formula)
+  ret <- list(coefficients=beta, lambda=lambda, formula=formula)
   class(ret) <- "ridge_reg"
   ret
 }
